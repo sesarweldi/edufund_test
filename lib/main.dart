df161216c995151d8025df97e14a6b2f7f96bcdf
Sesar Weldi <sesar@happy5.co>
2024-03-15 13:04:52 +0700

    config: initialize routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
+import 'package:edufund_test/config/routes/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   MyApp({super.key});
 
+ final _appRouter = AppRouter();
+ 
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(375, 812),
       splitScreenMode: true,
       builder: (context, child) {
-        return MaterialApp(
+        return MaterialApp.router(
+          routerConfig: _appRouter.config(),
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
             fontFamily: "Poppins",
---
+++ lib/presentation/search/page/search_page.dart
@@ -0,0 +1,17 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
